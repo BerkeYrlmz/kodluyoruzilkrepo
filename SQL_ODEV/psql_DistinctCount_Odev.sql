3cb385c8b3a0d4e74eb107a04a58753e31841e63
Berke Yorulmaz <berkeyorulmaz@hotmail.com>
2023-08-18 22:33:19 +0300

    Distinct-Count Odev Eklendi

---
+++ SQL_ODEV/psql_DistinctCount_Odev.sql
@@ -0,0 +1,20 @@
+--1.soru
+SELECT DISTINCT replacement_cost FROM film;
+
+--2.soru
+SELECT COUNT (DISTINCT replacement_cost) FROM film;
+
+--3.soru
+SELECT COUNT (*)
+FROM film
+WHERE title LIKE 'T%' AND rating = 'G';
+
+--4.soru
+SELECT COUNT (*)
+FROM country
+WHERE country LIKE '_____';
+
+--5.soru
+SELECT COUNT (*)
+FROM city
+WHERE city ILIKE '%R';
