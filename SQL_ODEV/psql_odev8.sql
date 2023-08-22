5536c595344fbb607e88ad8ca11dcb7ccb03651b
Berke Yorulmaz <berkeyorulmaz@hotmail.com>
2023-08-22 17:00:40 +0300

    psql-odev8 eklendi

---
+++ SQL_ODEV/psql_odev8.sql
@@ -0,0 +1,44 @@
+UPDATE employee
+SET name = 'Berke',
+email = 'berkeyorulmaz@hotmail.com',
+birthday = '1994-11-28'
+WHERE id = 1;
+
+UPDATE employee
+SET name = 'Umut',
+email = 'umutoku@gmail.com',
+birthday = '1993-09-13'
+WHERE id = 2
+
+UPDATE employee
+SET name = 'Tolgacan',
+email = 'tolgacanozdemir@gmail.com',
+birthday = '1994-06-3'
+WHERE id = 3
+
+UPDATE employee
+SET name = 'KocaSinan',
+email = 'kocasinan@gmail.com',
+birthday = '1993-01-06'
+WHERE id = 4
+
+UPDATE employee
+SET name = 'Emre',
+email = 'emresoylu@gmail.com',
+birthday = '1993-08-17'
+WHERE id = 5
+
+DELETE FROM employee
+WHERE id = 50
+
+DELETE FROM employee
+WHERE id = 49
+
+DELETE FROM employee
+WHERE id = 48
+
+DELETE FROM employee
+WHERE id = 47
+
+DELETE FROM employee
+WHERE id = 46
